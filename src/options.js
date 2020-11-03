d6c5d040f68537514204bf7bb3af7534fb2ac9e2
Floha <floha2011@hotmail.de>
2020-11-03 21:44:51 +0100

    Getting there

--- src/options.js
+++ src/options.js
@@ -7,15 +7,28 @@ export default class Options extends React.Component {
         return (
             <Form>
                 <FormGroup>
-                    <FormCheck type={"checkbox"} label={"Open Thunderhead"} id={"oth"}/>
-                    <FormCheck type={"checkbox"} label={"Swordless"} id={"swordless"}/>
-                    <FormCheck type={"checkbox"} label={"Tablet Randomizer"} id={"trando"}/>
+                    <FormCheck
+                        type={"checkbox"}
+                        label={"Open Thunderhead"}
+                        id={"oth"}
+                        checked={this.state.options.openThunderhead}
+                        onChange={this.changeOpenThunderhead()}
+                    />
+                    <FormCheck type={"checkbox"} label={"Swordless"} id={"swordless"} checked={this.state.options.swordless}/>
+                    <FormCheck type={"checkbox"} label={"Tablet Randomizer"} id={"trando"} checked={this.state.options.tabletRando}/>
+                    <FormCheck type={"checkbox"} label={"Race Mode"} id={"racemode"} checked={this.state.options.raceMode}/>
                 </FormGroup>
                 <FormGroup>
                     <FormLabel>Required Dungeons</FormLabel>
                 </FormGroup>
                 <FormGroup>
-                    <FormCheck type={"checkbox"} inline label={"Skyview Temple"} id={"sv"}/>
+                    <FormCheck
+                        type={"checkbox"}
+                        inline
+                        label={"Skyview Temple"}
+                        id={"sv"}
+                        checked={this.state.options.requiredDungeons.includes("Skyview")}
+                    />
                     <FormCheck type={"checkbox"} inline label={"Earth Temple"} id={"et"}/>
                     <FormCheck type={"checkbox"} inline label={"Lanayru Mining Facility"} id={"lmf"}/>
                     <FormCheck type={"checkbox"} inline label={"Ancient Cistern"} id={"ac"}/>
@@ -23,11 +36,34 @@ export default class Options extends React.Component {
                     <FormCheck type={"checkbox"} inline label={"Fire Sanctuary"} id={"fs"}/>
                     <FormCheck type={"checkbox"} disabled defaultChecked inline label={"Skykeep"} id={"sk"}/>
                 </FormGroup>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={this.submit()}>
                     Start Randomizer with these options
                 </Button>
             </Form>
         )
     }
+    
+    changeOpenThunderhead() {
+        let newstate = this.state.options
+        newstate.openThunderhead = !this.state.options.openThunderhead
+        this.setState({options: newstate})
+    }
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: {
+                requiredDungeons: [],
+                swordless: false,
+                openThunderhead: false,
+                tabletRando: false,
+                raceMode: false
+            }
+        }
+    }
+    
+    submit() {
+    
+    };
 
 }
