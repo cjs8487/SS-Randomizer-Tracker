2801538fb4f956f27d3f9043728546ee6278f4a0
cjs8487 <cjschaefer1999@gmail.com>
2021-01-07 09:43:34 -0500

    converted sword block to use single item component

---
+++ src/itemTracker/Item.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import _ from 'lodash'
+
+class Item extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.onChange(this.props.itemName);
+    }
+
+    render() {
+        const current = this.props.logic.getItem(this.props.itemName)
+        return (
+            <div className="item" onClick={this.handleClick}>
+                <img src={this.props.images[current]} alt={this.props.itemName} width={this.props.imgWidth} />
+            </div>
+        )
+    }
+}
+
+export default Item;
