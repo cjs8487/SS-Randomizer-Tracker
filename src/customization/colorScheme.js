1f927e3b396664d7b316d652d81452f404f2226a
cjs8487 <cjschaefer1999@gmail.com>
2021-01-25 23:50:00 -0500

    fixed customization package

--- src/customization/colorScheme.js
+++ src/customization/colorScheme.js
@@ -1,13 +1,13 @@
 class ColorScheme {
     constructor() {
-        this.outLogic = "#FF0000"
-        this.inLogic = "#008000"
-        this.semiLogic = "#FFA500"
-        this.background = "#FFFFFF"
-        this.text = "#000000"
-        this.required = "#008000"
-        this.unrequired = "#808080"
-        this.checked = "#303030"
+        this.outLogic = '#FF0000';
+        this.inLogic = '#008000';
+        this.semiLogic = '#FFA500';
+        this.background = '#FFFFFF';
+        this.text = '#000000';
+        this.required = '#008000';
+        this.unrequired = '#808080';
+        this.checked = '#303030';
     }
 }
 
