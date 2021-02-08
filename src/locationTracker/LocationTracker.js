04ebbd29bff84c69991fd80482c6142a1ef7e8e2
cjs8487 <cjschaefer1999@gmail.com>
2021-02-08 00:23:21 -0500

    tweaked the location tracker

--- src/locationTracker/LocationTracker.js
+++ src/locationTracker/LocationTracker.js
@@ -1,32 +1,62 @@
 import React from 'react';
+import _ from 'lodash';
 import PropTypes from 'prop-types';
 import LocationGroup from './LocationGroup';
+import AreaCounters from './AreaCounters';
 import './locationTracker.css';
 import ColorScheme from '../customization/ColorScheme';
 import Logic from '../logic/Logic';
 
 class LocationTracker extends React.Component {
+    constructor(props) {
+        super(props);
+        _.forEach(this.props.logic.areas(), (area) => {
+            this[_.camelCase(`open${area}`)] = this.openLocation.bind(this, area);
+        });
+    }
+
+    openLocation(location) {
+        this.props.handleGroupClick(location);
+    }
+
     render() {
         return (
             <div className="location-tracker">
-                <ul style={{ padding: '2%' }}>
-                    {
-                        this.props.logic.areas().map((value) => (
+                <div>
+                    <ul style={{ padding: '2%' }}>
+                        {
+                            this.props.logic.areas().map((value) => (
+                                <div className="group-container" onClick={this[_.camelCase(`open${value}`)]} onKeyDown={this.onClick} role="button" tabIndex="0">
+                                    <h3 style={{ cursor: 'pointer', color: this.props.colorScheme.text }}>
+                                        {value}
+                                        <AreaCounters
+                                            totalChecksLeftInArea={this.props.logic.getTotalCountForArea(value)}
+                                            totalChecksAccessible={this.props.logic.getInLogicCountForArea(value)}
+                                            colorScheme={this.props.colorScheme}
+                                        />
+                                    </h3>
+                                </div>
+                            ))
+                        }
+                    </ul>
+                </div>
+                {
+                    this.props.expandedGroup && (
+                        <div>
                             <LocationGroup
-                                key={value}
-                                groupName={value}
-                                locations={this.props.logic.locationsForArea(value)}
-                                expanded={this.props.expandedGroup === value}
+                                groupName={this.props.expandedGroup}
+                                locations={this.props.logic.locationsForArea(this.props.expandedGroup)}
+                                expanded
                                 handler={this.props.handleGroupClick}
                                 locationHandler={this.props.handleLocationClick}
-                                remainingChecks={this.props.logic.getTotalCountForArea(value)}
-                                inLogicChecks={this.props.logic.getInLogicCountForArea(value)}
+                                remainingChecks={this.props.logic.getTotalCountForArea(this.props.expandedGroup)}
+                                inLogicChecks={this.props.logic.getInLogicCountForArea(this.props.expandedGroup)}
                                 meetsRequirement={this.props.logic.isRequirementMet}
                                 colorScheme={this.props.colorScheme}
                             />
-                        ))
-                    }
-                </ul>
+                        </div>
+                    )
+                }
             </div>
         );
     }
