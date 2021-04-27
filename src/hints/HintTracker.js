958f48e3708a30406f7e43c171c8ee9d47fd240b
cjs8487 <cjschaefer1999@gmail.com>
2021-04-26 20:50:49 -0400

    persistent hints

--- src/hints/HintTracker.js
+++ src/hints/HintTracker.js
@@ -8,6 +8,23 @@ import hintableLocations from '../data/hintablelocations.json';
 import hintItems from '../data/hintItems.json';
 
 class HintTracker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hints: {},
+        };
+    }
+
+    locationChanged(hint, location) {
+        _.set(this.state.hints, [hint, 'location'], location);
+        this.forceUpdate();
+    }
+
+    itemChanged(hint, item) {
+        _.set(this.state.hints, [hint, 'item'], item);
+        this.forceUpdate();
+    }
+
     render() {
         const style = { background: this.props.colorScheme.background, color: this.props.colorScheme.text };
         return (
@@ -27,24 +44,24 @@ class HintTracker extends React.Component {
                                     </Col>
                                     <Col>
                                         <Form.Label>Hinted Location</Form.Label>
-                                        <Form.Control as="select">
+                                        <Form.Control as="select" onChange={(e) => this.locationChanged(hintLocation.name, e.target.value)}>
                                             <option>Unknown</option>
                                             {
                                                 _.map(hintableLocations, (location) => (
-                                                    <option>{location}</option>
+                                                    <option selected={_.get(this.state.hints, [hintLocation.name, 'location']) === location}>{location}</option>
                                                 ))
                                             }
                                         </Form.Control>
                                     </Col>
                                     <Col>
                                         <Form.Label>Hinted Item</Form.Label>
-                                        <Form.Control as="select">
+                                        <Form.Control as="select" onChange={(e) => this.itemChanged(hintLocation.name, e.target.value)}>
                                             {
                                                 _.map(hintItems, (items, groupName) => (
                                                     <optgroup label={groupName}>
                                                         {
                                                             _.map(items, (item) => (
-                                                                <option>{item}</option>
+                                                                <option selected={_.get(this.state.hints, [hintLocation.name, 'item']) === item}>{item}</option>
                                                             ))
                                                         }
                                                     </optgroup>
