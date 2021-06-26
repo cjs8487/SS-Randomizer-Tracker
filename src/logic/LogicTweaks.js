65fe91786db4eeabe01e8f9de094a65745a5d7cf
cjs8487 <cjschaefer1999@gmail.com>
2021-06-25 23:53:27 -0400

    unshuffled dungeon entrances properly set macro for dungeon in lanayru beaten

--- src/logic/LogicTweaks.js
+++ src/logic/LogicTweaks.js
@@ -19,11 +19,12 @@ class LogicTweaks {
             // no entrance randomizer, sub default requirements in
             requirements.set('Can Access Skyview', 'Can Access Dungeon Entrance in Deep Woods');
             requirements.set('Can Access Earth Temple', 'Can Access Dungeon Entrance in Eldin Volcano');
-            requirements.set('Can Access Lanayru Mining Facility', 'Can Access Dungeon Entrance in Lanayru Desert');
+            requirements.set('Can Access Lanayru Mining Facility', 'Can Access Dungeon Entrance In Lanayru Desert');
             requirements.set('Can Access Ancient Cistern', 'Can Access Dungeon Entrance in Lake Floria');
             requirements.set('Can Access Sandship', 'Can Access Dungeon Entrance in Sand Sea');
             requirements.set('Can Access Fire Sanctuary', 'Can Access Dungeon Entrance in Volcano Summit');
             requirements.set('Can Access Sky Keep', 'Can Access Dungeon Entrance on Skyloft');
+            requirements.set('Can Beat Dungeon Entrance In Lanayru Desert', 'Can Beat Lanayru Mining Facility');
         } else {
             requirements.set('Can Access Skyview', 'Entered Skyview');
             requirements.set('Can Access Earth Temple', 'Entered Earth Temple');
