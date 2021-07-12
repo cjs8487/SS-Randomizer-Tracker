bde389d7b45ad0632f22e8d2e2cc6026bb98257d
cjs8487 <cjschaefer1999@gmail.com>
2021-07-11 22:38:20 -0400

    Converted to standalone page

--- src/entranceTracker/EntranceTracker.js
+++ src/entranceTracker/EntranceTracker.js
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Modal, Button } from 'react-bootstrap';
+// import { Modal, Button } from 'react-bootstrap';
 import EntranceGraph from './EntranceGraph';
 
 class EntranceTracker extends React.Component {
     render() {
         return (
-            <Modal show>
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Tracker Customization
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="show-grid">
-                    <EntranceGraph />
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button>Close</Button>
-                </Modal.Footer>
-            </Modal>
+            // <Modal show>
+            //     <Modal.Header closeButton>
+            //         <Modal.Title id="contained-modal-title-vcenter">
+            //             Tracker Customization
+            //         </Modal.Title>
+            //     </Modal.Header>
+            //     <Modal.Body className="show-grid">
+            //         <EntranceGraph />
+            //     </Modal.Body>
+            //     <Modal.Footer>
+            //         <Button>Close</Button>
+            //     </Modal.Footer>
+            // </Modal>
+            <EntranceGraph />
         );
     }
 }
