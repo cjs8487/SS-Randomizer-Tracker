719fd39fc6842fc6497dde52e295e5334f19ffcd
cjs8487 <cjschaefer1999@gmail.com>
2021-10-12 17:59:19 -0400

    oops forgot to change option fetching

--- src/permalink/Settings.js
+++ src/permalink/Settings.js
@@ -109,7 +109,7 @@ class Settings {
     }
 
     async loadSettingsFromRepo() {
-        const response = await fetch('https://raw.githubusercontent.com/lepelog/sslib/gui-redesign/options.yaml');
+        const response = await fetch('https://raw.githubusercontent.com/lepelog/sslib/master/options.yaml');
         const text = await response.text();
         this.allOptions = yaml.safeLoad(text);
     }
