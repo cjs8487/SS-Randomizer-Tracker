ffabfd15c81d7f0ee731989b90bab2d4b1cdc61f
cjs8487 <cjschaefer1999@gmail.com>
2022-03-02 17:22:05 -0500

    sorted the exit list

--- src/entranceTracker/EntranceTracker.js
+++ src/entranceTracker/EntranceTracker.js
@@ -11,7 +11,7 @@ class EntranceTracker extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            entrances: {},
+            exits: {},
             selected: {},
         };
         this.onEntranceChange = this.onEntranceChange.bind(this);
@@ -33,13 +33,20 @@ class EntranceTracker extends React.Component {
     async fetchEntranceList() {
         const response = await fetch('https://raw.githubusercontent.com/ssrando/ssrando/fc38600187f45d0de04ffe9d769758f812df663e/entrance_table2.yaml');
         const text = await response.text();
-        const entrances = await yaml.load(text);
-        this.setState({ entrances });
+        const exits = await yaml.load(text);
+        const mappedExits = {};
+        // _.forEach(exits, (exit) => {
+        //     if (!mappedExits[exit.stage]) {
+        //         mappedExits[exit.stage] = [];
+        //     }
+        //     mappedExits[exit.stage].push(`to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`);
+        // });
+        this.setState({ exits: _.sortBy(exits, (exit) => exit.stage) });
     }
 
     row({ index }) {
-        const { entrances, selected } = this.state;
-        const exit = entrances[index];
+        const { exits, selected } = this.state;
+        const exit = exits[index];
         const exitText = `${exit.stage} to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`;
         return (
             <Row key={exitText}>
@@ -49,7 +56,7 @@ class EntranceTracker extends React.Component {
                 <Col>
                     <select onChange={this.onEntranceChange} id={exitText}>
                         {
-                            _.map(entrances, (entrance) => {
+                            _.map(exits, (entrance) => {
                                 const entranceText = `${entrance['to-stage']} (from ${entrance.stage}${entrance.disambiguation ? `, ${entrance.disambiguation}` : ''}${entrance.door ? `, ${entrance.door} Door` : ''})`;
                                 if (selected[exitText] === entranceText) {
                                     return (<option key={entranceText} selected>{entranceText}</option>);
@@ -64,8 +71,7 @@ class EntranceTracker extends React.Component {
     }
 
     render() {
-        const { entrances } = this.state;
-        // console.log(entrances);
+        const { exits } = this.state;
         return (
             <Modal show={this.props.show} onHide={this.props.onHide}>
                 <Modal.Header closeButton>
@@ -74,7 +80,7 @@ class EntranceTracker extends React.Component {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body className="show-grid">
-                    <List itemCount={entrances.length} height={400} itemSize={25}>
+                    <List itemCount={exits.length} height={400} itemSize={25}>
                         {this.row}
                     </List>
                 </Modal.Body>
