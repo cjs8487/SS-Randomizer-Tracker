492e1a71755b6ccd521426bb525f38e7bd1aedca
cjs8487 <cjschaefer1999@gmail.com>
2022-03-02 13:42:10 -0500

    basic entrance list

--- src/entranceTracker/EntranceTracker.js
+++ src/entranceTracker/EntranceTracker.js
@@ -1,26 +1,70 @@
+import _ from 'lodash';
 import React from 'react';
-// import { Modal, Button } from 'react-bootstrap';
-import EntranceGraph from './EntranceGraph';
+import PropTypes from 'prop-types';
+import { Modal, Button, Row, Col } from 'react-bootstrap';
+import yaml from 'js-yaml';
+// import EntranceGraph from './EntranceGraph';
 
 class EntranceTracker extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            entrances: {},
+        };
+        this.fetchEntranceList();
+    }
+
+    async fetchEntranceList() {
+        const response = await fetch('https://raw.githubusercontent.com/ssrando/ssrando/fc38600187f45d0de04ffe9d769758f812df663e/entrance_table2.yaml');
+        const text = await response.text();
+        const data = yaml.load(text);
+        this.setState({ entrances: data });
+    }
+
     render() {
+        const { entrances } = this.state;
+        const selectElement = (
+            <select>
+                {
+                    _.map(entrances, (entrance) => (
+                        <option>{`${entrance['to-stage']} (from ${entrance.stage})`}</option>
+                    ))
+                }
+            </select>
+        );
         return (
-            // <Modal show>
-            //     <Modal.Header closeButton>
-            //         <Modal.Title id="contained-modal-title-vcenter">
-            //             Tracker Customization
-            //         </Modal.Title>
-            //     </Modal.Header>
-            //     <Modal.Body className="show-grid">
-            //         <EntranceGraph />
-            //     </Modal.Body>
-            //     <Modal.Footer>
-            //         <Button>Close</Button>
-            //     </Modal.Footer>
-            // </Modal>
-            <EntranceGraph />
+            <Modal show={this.props.show} onHide={this.props.onHide}>
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter">
+                        Entrances
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="show-grid">
+                    {
+                        _.map(entrances, (entrance) => (
+                            <Row>
+                                <Col>
+                                    {`${entrance.stage} to ${entrance['to-stage']}`}
+                                </Col>
+                                <Col>
+                                    {selectElement}
+                                </Col>
+                            </Row>
+                        ))
+                    }
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.props.onHide}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+            // <EntranceGraph />
         );
     }
 }
+
+EntranceTracker.propTypes = {
+    show: PropTypes.bool.isRequired,
+    onHide: PropTypes.func.isRequired,
+};
 
 export default EntranceTracker;
