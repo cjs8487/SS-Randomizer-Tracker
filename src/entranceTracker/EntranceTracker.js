78cf80a3e8e6099634efaf61eee48cb490fe247e
cjs8487 <cjschaefer1999@gmail.com>
2022-03-14 22:50:05 -0400

    converted entrance list to functional component for minor performance gain

--- src/entranceTracker/EntranceTracker.js
+++ src/entranceTracker/EntranceTracker.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Button, Row, Col, FormCheck } from 'react-bootstrap';
 import { FixedSizeList as List } from 'react-window';
@@ -7,122 +7,105 @@ import Select from 'react-select';
 import yaml from 'js-yaml';
 // import EntranceGraph from './EntranceGraph';
 
-class EntranceTracker extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            exits: [],
-            entrances: [],
-            displayedExits: {},
-            selected: {},
-            clickthrough: true,
-            // hiddenEntrances: [],
-        };
-        this.onEntranceChange = this.onEntranceChange.bind(this);
-        this.onSearchChange = this.onSearchChange.bind(this);
-        this.onOptionChange = this.onOptionChange.bind(this);
-        this.fetchEntranceList();
-    }
+function EntranceTracker(props) {
+    const [exits, setExits] = useState([]);
+    const [entrances, setEntrances] = useState([]);
+    const [displayedExits, setDisplayedExits] = useState({});
+    const [selected, setSelected] = useState({});
+    const [search, setSearch] = useState('');
+    const [clickthrough, setClickthrough] = useState(true);
 
-    onEntranceChange(selectedOption, meta) {
-        const { name } = meta;
-        this.setState((state) => {
-            const selected = { ...state.selected };
-            const { clickthrough } = state;
-            let { displayedExits } = state;
-            selected[name] = selectedOption;
-            let entrances = [...state.entrances];
-            const old = state.selected[name];
-            if (old) {
-                entrances.push({ value: old.value, label: old.label });
-                entrances = _.sortBy(entrances, (entrance) => entrance.value);
-            }
-            _.remove(entrances, (entrance) => entrance.value === selectedOption.value);
-
-            if (clickthrough) {
-                const toFilter = selectedOption.value.toLowerCase().split('(')[0];
-                displayedExits = _.filter(state.exits, (exit) => exit.exitText.toLowerCase().includes(toFilter));
-            }
-            return { selected, entrances, displayedExits };
-        });
-    }
-
-    onSearchChange(e) {
-        const { value } = e.target;
-        this.setState((state) => ({ displayedExits: _.filter(state.exits, (exit) => exit.exitText.toLowerCase().includes(value.toLowerCase())) }));
-    }
-
-    onOptionChange(e) {
-        const { id } = e.target;
-        if (id === 'clickthrough') {
-            this.setState((prevState) => ({ clickthrough: !prevState.clickthrough }));
+    // runs on mount
+    useEffect(() => {
+        async function fetchEntranceList() {
+            const response = await fetch('https://raw.githubusercontent.com/ssrando/ssrando/fc38600187f45d0de04ffe9d769758f812df663e/entrance_table2.yaml');
+            const text = await response.text();
+            // eslint-disable-next-line no-shadow
+            const exits = await yaml.load(text);
+            _.forEach(exits, (exit) => {
+                exit.exitText = `${exit.stage} to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`;
+                exit.entranceText = `${exit['to-stage']} (from ${exit.stage}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''})`;
+            });
+            const sorted = _.sortBy(exits, (exit) => exit.stage);
+            const entranceList = [];
+            _.forEach(exits, (exit) => {
+                entranceList.push({ value: exit.entranceText, label: exit.entranceText });
+            });
+            const sortedEntrances = _.sortBy(entranceList, (entrance) => entrance.value);
+            setExits(sorted);
+            setDisplayedExits(sorted);
+            setEntrances(sortedEntrances);
         }
-    }
+        fetchEntranceList();
+    }, []);
 
-    async fetchEntranceList() {
-        const response = await fetch('https://raw.githubusercontent.com/ssrando/ssrando/fc38600187f45d0de04ffe9d769758f812df663e/entrance_table2.yaml');
-        const text = await response.text();
-        const exits = await yaml.load(text);
-        _.forEach(exits, (exit) => {
-            exit.exitText = `${exit.stage} to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`;
-            exit.entranceText = `${exit['to-stage']} (from ${exit.stage}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''})`;
-        });
-        const sorted = _.sortBy(exits, (exit) => exit.stage);
-        const entranceList = [];
-        _.forEach(exits, (exit) => {
-            entranceList.push({ value: exit.entranceText, label: exit.entranceText });
-        });
-        const sortedEntrances = _.sortBy(entranceList, (entrance) => entrance.value);
-        this.setState({ exits: sorted, displayedExits: sorted, entrances: sortedEntrances });
-    }
+    // onEntranceChange
+    const onEntranceChange = (selectedOption, meta) => {
+        const { name } = meta;
+        selected[name] = selectedOption;
+        setSelected(selected);
+        const old = selected[name];
+        if (old) {
+            entrances.push({ value: old.value, label: old.label });
+        }
+        _.remove(entrances, (entrance) => entrance.value === selectedOption.value);
+        setEntrances(_.sortBy(entrances, (entrance) => entrance.value));
 
-    render() {
-        const { displayedExits, selected, entrances } = this.state;
+        if (clickthrough) {
+            const toFilter = selectedOption.value.toLowerCase().split('(')[0];
+            // displayedExits = _.filter(exits, (exit) => exit.exitText.toLowerCase().includes(toFilter));
+            setSearch(toFilter);
+        }
+    };
 
-        const row = ({ index, style }) => {
-            const exit = displayedExits[index];
-            const exitText = `${exit.stage} to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`;
-            return (
-                <Row key={exit.exitText} style={{ ...style, borderBottom: '1px solid black', paddingTop: '1%' }}>
-                    <Col>
-                        {exit.exitText}
-                    </Col>
-                    <Col>
-                        <Select value={selected[exitText]} onChange={this.onEntranceChange} options={entrances} name={exitText} />
-                    </Col>
-                </Row>
-            );
-        };
+    // onSearchChange
+    useEffect(() => {
+        setDisplayedExits(_.filter(exits, (exit) => exit.exitText.toLowerCase().includes(search.toLowerCase())));
+    }, [search]);
+
+    const row = ({ index, style }) => {
+        const exit = displayedExits[index];
+        const exitText = `${exit.stage} to ${exit['to-stage']}${exit.disambiguation ? `, ${exit.disambiguation}` : ''}${exit.door ? `, ${exit.door} Door` : ''}`;
         return (
-            <Modal show={this.props.show} onHide={this.props.onHide} size="lg" style={{ width: '90%' }}>
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        Entrances
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body className="show-grid">
-                    <div style={{ paddingBottom: '3%' }}>
-                        <input type="search" placeholder="Search entrances" onChange={this.onSearchChange} />
-                        <FormCheck
-                            type="switch"
-                            label="Clickthrough"
-                            id="clickthrough"
-                            checked={this.state.clickthrough}
-                            onChange={this.onOptionChange}
-                        />
-                        <Button onClick={() => this.setState((prevState) => ({ displayedExits: prevState.exits }))}>Clear Filters</Button>
-                    </div>
-                    <List itemCount={displayedExits.length} height={600} itemSize={60}>
-                        {row}
-                    </List>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={this.props.onHide}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-            // <EntranceGraph />
+            <Row key={exit.exitText} style={{ ...style, borderBottom: '1px solid black', paddingTop: '1%' }}>
+                <Col>
+                    {exit.exitText}
+                </Col>
+                <Col>
+                    <Select value={selected[exitText]} onChange={onEntranceChange} options={entrances} name={exitText} />
+                </Col>
+            </Row>
         );
-    }
+    };
+    return (
+        <Modal show={props.show} onHide={props.onHide} size="lg" style={{ width: '90%' }}>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Entrances
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body className="show-grid">
+                <div style={{ paddingBottom: '3%' }}>
+                    <input type="search" placeholder="Search entrances" onChange={(e) => setSearch(e.target.value)} value={search} />
+                    <FormCheck
+                        type="switch"
+                        label="Clickthrough"
+                        id="clickthrough"
+                        checked={clickthrough}
+                        onChange={() => setClickthrough(!clickthrough)}
+                    />
+                    <Button onClick={() => setSearch('')}>Clear Filters</Button>
+                </div>
+                <List itemCount={displayedExits.length} height={600} itemSize={60}>
+                    {row}
+                </List>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+        // <EntranceGraph />
+    );
 }
 
 EntranceTracker.propTypes = {
