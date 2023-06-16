f064dbacecfbf966b08ebfd31fb43000f7030eb5
cjs8487 <cjschaefer1999@gmail.com>
2023-06-15 19:16:02 -0500

    fix scaling in quest items

--- src/itemTracker/QuestItems.tsx
+++ src/itemTracker/QuestItems.tsx
@@ -27,73 +27,61 @@ const QuestItems = (props: QuestItemProps) => {
     return (
         <div id="quest-items">
             <img src={questItemBlock} alt="" width={width} />
-            <div
-                style={{
-                    position: 'relative',
-                    bottom: width * 0.18 + 900 / width,
-                    left: width / 14,
-                }}
-            >
-                <Item
-                    itemName="Cawlin's Letter"
-                    logic={props.logic}
-                    onChange={props.handleItemClick}
-                    imgWidth={letterWidth}
-                />
-            </div>
-            <div
-                style={{
-                    position: 'relative',
-                    bottom: width * 0.2 + 300 / width,
-                    left: width / 3.26,
-                }}
-            >
-                <Item
-                    itemName="Horned Colossus Beetle"
-                    logic={props.logic}
-                    onChange={props.handleItemClick}
-                    imgWidth={cBeetleWidth}
-                />
-            </div>
-            <div
-                style={{
-                    position: 'relative',
-                    bottom: width * 0.205 + 200 / width,
-                    left: width / 1.85,
-                }}
-            >
-                <Item
-                    itemName="Baby Rattle"
-                    logic={props.logic}
-                    onChange={props.handleItemClick}
-                    imgWidth={rattleWidth}
-                />
-            </div>
-            <div
-                style={{
-                    position: 'relative',
-                    bottom: width * 0.19 + 400 / width,
-                    left: width / 1.26,
-                }}
-            >
-                <GratitudeCrystals
-                    logic={props.logic}
-                    onChange={props.handleItemClick}
-                    imgWidth={crystalWidth}
-                />
-            </div>
-            <div
-                style={{
-                    position: 'relative',
-                    bottom: width * 0.15,
-                    left: width * 0.9,
-                }}
-            >
-                <CrystalCounter
-                    current={props.logic.getCrystalCount()}
-                    colorScheme={props.colorScheme}
-                    fontSize={crystalWidth * 1.25}
-                />
+            <div style={{ position: 'absolute', top: 0 }}>
+                <div
+                    style={{
+                        position: 'relative',
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                        width,
+                        paddingLeft: width / 12,
+                        paddingRight: width / 7,
+                        transform: 'translate(0, 25%)',
+                    }}
+                >
+                    <Item
+                        itemName="Cawlin's Letter"
+                        logic={props.logic}
+                        onChange={props.handleItemClick}
+                        imgWidth={letterWidth}
+                        ignoreItemClass
+                    />
+                    <Item
+                        itemName="Horned Colossus Beetle"
+                        logic={props.logic}
+                        onChange={props.handleItemClick}
+                        imgWidth={cBeetleWidth}
+                        ignoreItemClass
+                    />
+                    <Item
+                        itemName="Baby Rattle"
+                        logic={props.logic}
+                        onChange={props.handleItemClick}
+                        imgWidth={rattleWidth}
+                        ignoreItemClass
+                    />
+
+                    <div>
+                        <GratitudeCrystals
+                            logic={props.logic}
+                            onChange={props.handleItemClick}
+                            imgWidth={crystalWidth}
+                        />
+                        <div
+                            style={{
+                                position: 'relative',
+                                bottom: '-10%',
+                                left: '90%',
+                            }}
+                        >
+                            <CrystalCounter
+                                current={props.logic.getCrystalCount()}
+                                colorScheme={props.colorScheme}
+                                fontSize={crystalWidth * 1.25}
+                            />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
